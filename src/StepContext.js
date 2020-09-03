5e9a914bd5b79a7caa7464cbb75ccaecfcf0e1d1
Adedipupo <bayonleolaluwa@gmail.com>
2020-09-03 14:09:52 +0100

    wip

---
+++ src/StepContext.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const StepContext = () => {
+  const [ currentStep, setStep] = useState(1)
+return(
+  <div>
+     
+  </div>
+)
+}
+
+export default StepContext
