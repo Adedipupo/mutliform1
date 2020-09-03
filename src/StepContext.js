092ea6658b68c950010160052e6824c9c51b5f9a
Adedipupo <bayonleolaluwa@gmail.com>
2020-09-03 14:17:27 +0100

    wip

--- src/StepContext.js
+++ src/StepContext.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
 
+
+export const mutliStepContext = React.createContext();
 const StepContext = () => {
-  const [ currentStep, setStep] = useState(1)
+  const [ currentStep, setStep] = useState(1);
+  const [ userData, setUserData] = ([]);
+  const [ finalData, setFinalData] = ([]);
 return(
   <div>
-     
+     <mutliStepContext.Provider value={{currentStep,setStep,userData,setUserData,finalData,setFinalData}}>
+
+     </mutliStepContext.Provider>
   </div>
 )
 }
